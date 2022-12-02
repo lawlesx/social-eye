84218d979bb2874262e29274babca7760063494d
Aniruddha Sil <welcometohellv@gmail.com>
2022-12-03 01:15:07 +0530

    setup bg and eslint and prettier

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,33 @@
 import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+// import Image from 'next/image'
 
 export default function Home() {
   return (
-    <div className='w-full h-full bg-purple-300'>
-      <h1 className='text-white text-[10rem] font-black'>Social Eye</h1>
-    </div>
+    <div className='w-full bg-[url("/images/bg.svg")] bg-fixed bg-no-repeat'>
+      <Head>
+        <title>Social Eye</title>
+        <meta
+          name="description"
+          content="Social graph based on Lens protocol"
+        />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      {/* <div className='w-full h-full fixed top-0' style={{ zIndex: -1 }}>
+        <Image
+          src="/images/noise.png"
+          alt="noise"
+          fill
+          style={{ objectFit: 'cover' }}
+        />
+      </div> */}
+      <h1 className="text-fuchsia-300 text-[10rem] font-black z-10">Social Eye</h1>
+      <h1 className="text-fuchsia-300 text-[10rem] font-black">Social Eye 2</h1>
+      <h1 className="text-fuchsia-300 text-[10rem] font-black">Social Eye 3</h1>
+      <h1 className="text-fuchsia-300 text-[10rem] font-black">Social Eye 4</h1>
+      <h1 className="text-fuchsia-300 text-[10rem] font-black">Social Eye 5</h1>
+      <h1 className="text-fuchsia-300 text-[10rem] font-black">Social Eye 6</h1>
+      <h1 className="text-fuchsia-300 text-[10rem] font-black">Social Eye 7</h1>
+      <h1 className="text-fuchsia-300 text-[10rem] font-black">Social Eye 8</h1>
+    </div >
   )
 }
