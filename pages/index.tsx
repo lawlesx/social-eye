6825af98415f3366a1de6ae34f724df3c3d1b712
Aniruddha Sil <welcometohellv@gmail.com>
2022-12-03 16:02:10 +0530

    update bg stickers

--- pages/index.tsx
+++ pages/index.tsx
@@ -23,13 +23,14 @@ export default function Home() {
         />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <Image src="/images/sun.svg" alt="Sun" width={250} height={250} className="absolute left-20" />
+      <Image src="/images/sun.svg" alt="Sun" width={200} height={200} className="absolute left-20" />
       <Image src="/images/gear.svg" alt="Gear" width={65} height={65} className="absolute left-0 top-40" />
-      <Image src="/images/flower.svg" alt="Flower" width={180} height={180} className="absolute left-0 bottom-10" />
-      <Image src="/images/PinkSparkle.svg" alt="Pink Sparkle" width={50} height={50} className="absolute right-[10rem] top-10" />
+      <Image src="/images/FallingStar.svg" alt="Falling Star" width={200} height={200} className="absolute left-0 bottom-10" />
+      <Image src="/images/ColourfulFire.svg" alt="Colourful Fire" width={50} height={50} className="absolute right-[12rem] top-4" />
       <Image src="/images/ColorArrow.svg" alt="Color Arrow" width={80} height={80} className="absolute left-[17rem] top-[55%]" />
       <Image src="/images/GoldDiamond.svg" alt="Gold Diamond" width={65} height={65} className="absolute right-[20rem] top-[40%]" />
-      <Image src="/images/BlueYellowSparkle.svg" alt="Blue Yellow sparkle" width={40} height={40} className="absolute right-[5rem] top-40" />
+      <Image src="/images/BlueYellowFluffy.svg" alt="Blue Yellow Fluffy" width={45} height={45} className="absolute right-[2rem] top-40" />
+      <Image src="/images/dreams.svg" alt="Dreams" width={120} height={120} className="absolute right-[2rem] top-10" />
       <div className='w-full h-screen flex flex-col items-center gap-4 justify-start pt-10'>
         <div className='w-[15rem] h-[15rem] relative'>
           <Image src="/images/eye.svg" alt="Eye" fill style={{ objectFit: 'contain' }} />
--- pages/nft.tsx
+++ pages/nft.tsx
@@ -1,18 +1,28 @@
 import Head from 'next/head'
+import Image from 'next/image'
 import React, { FC } from 'react'
+import NavArrowIcon from '../components/NavArrowIcon'
 
 const Nft: FC = () => {
   return (
     <div className="h-screen">
       <Head>
         <title>Social Eye | NFT</title>
-        <meta
-          name="description"
-          content="Social graph based on Lens protocol"
-        />
+        <meta name="description" content="NFTs of Lens Profile" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      Nft
+      <div className="w-full h-1/4 bg-purple-400 border-b-4 border-black relative">
+        <Image
+          src="/images/cover.png"
+          alt="Cover"
+          fill
+          style={{ objectFit: 'cover' }}
+        />
+        <button className="absolute font-bold text-sm flex items-center gap-2 mt-6 ml-6">
+          <NavArrowIcon className="w-6 h-6" direction="prev" />
+          Go back
+        </button>
+      </div>
     </div>
   )
 }
