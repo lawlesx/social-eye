7c5b2b09925aa1de4ce4f9293ab77f1dd7081846
Aniruddha Sil <welcometohellv@gmail.com>
2022-12-03 10:46:40 +0530

    added search bar

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,7 +2,9 @@ import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <div className='w-full bg-[url("/images/bg.svg")] bg-fixed bg-no-repeat relative'>
-    <Component {...pageProps} />
-  </div>
+  return (
+    <div className='w-full bg-[url("/images/bg.svg")] bg-fixed bg-no-repeat relative bg-cover'>
+      <Component {...pageProps} />
+    </div>
+  )
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -17,8 +17,12 @@ export default function Home() {
         <div className='w-40 h-40 relative'>
           <Image src="/images/eye.svg" alt="Eye" fill style={{ objectFit: 'contain' }} />
         </div>
-        {/* <h1 className='text-[8rem] text-[#36028F]'>Social Eye</h1> */}
         <DazedText>Social Eye</DazedText>
+        <p className='font-light text-2xl w-2/3 text-center text-gray-800'>Integer tincidunt amet integer nunc sit pellentesque urna vestibulum. Enim amet sit sagittis lacus.</p>
+        <div className='w-3/5 h-14 rounded-full flex items-center justify-between border-2 border-black overflow-hidden'>
+          <input className='outline-none h-full font-light text-gray-600 text-2xl placeholder:text-gray-500 w-full bg-transparent px-8' placeholder='e.g adam.lens' />
+          <button className='w-1/3 h-full bg-[#B39AFF] text-white font-light text-2xl rounded-l-full outline outline-black'>Search</button>
+        </div>
       </div>
     </div>
   )
