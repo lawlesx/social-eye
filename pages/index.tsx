68a9e3cdded2e89884d0481fcbc6c52726c41df3
Aniruddha Sil <welcometohellv@gmail.com>
2022-12-04 04:13:28 +0530

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -24,7 +24,7 @@ export default function Home() {
           <Image src="/images/eye.svg" alt="Eye" fill style={{ objectFit: 'contain' }} />
         </div>
         <DazedText>Social Eye</DazedText>
-        <p className='font-light text-2xl w-2/3 text-center text-gray-800 py-4'>Integer tincidunt amet integer nunc sit pellentesque urna vestibulum. Enim amet sit sagittis lacus.</p>
+        <p className='font-light text-2xl w-2/3 text-center text-gray-800 py-4'>{`Would you like to know how you can visualize your connections? Well, now you can. It's as simple as entering your Len's ID.`}</p>
         <div className='w-3/5 h-14 rounded-full flex items-center justify-between border-2 border-black my-4'>
           <input onChange={(e) => setHandle(e.target.value)} className='outline-none h-full font-light text-gray-600 text-2xl placeholder:text-gray-500 w-full bg-transparent px-8' placeholder='e.g adam.lens' />
           <Link href={`/${handle}`} className='w-1/3 flex items-center justify-center h-full bg-[#B39AFF] text-black font-normal hover:bg-[#a384fe] hover:text-white transition-colors ease-in-out text-xl rounded-full outline outline-2 outline-black'>Search</Link>
